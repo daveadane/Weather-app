6817dd342fa37c416623a2a4890211d83016e551
Dawit Adane <dawitadane.woldekirkose@yh.nackademin.se>
2026-01-03 15:12:15 +0100

    Fixed the error on alphabet

--- src/Food.jsx
+++ src/Food.jsx
@@ -1,19 +1,2 @@
 
-function Food() {
-    
-     const food1 = "Pizza"
-     const food2 = "Sushi"
-     const food3 = "Tacos"
 
-     return(
-        <ul>
-            <li>Apple</li>
-            <li>{food2}</li>
-            <li>{food3.toUpperCase()}</li>
-        </ul>
-     );
-
-
-
-}
-export default Food
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,2 @@
 
 
-
-function Header() {
-    return (
-        <header>
-            <h1>Welcome to My React App</h1>
-            <nav>
-                <ul>
-                    <li><a href="/"> Home</a></li>
-                    <li><a href="/about"> About</a></li>
-                    <li><a href="/services"> Services</a></li>
-                    <li><a href="/contact"> Contact</a></li>
-
-                </ul>
-            </nav>
-            <hr></hr>    
-                    
-        </header>
-    );
-}
-export default Header;
